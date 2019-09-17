379c72dafab70af0a39577e8f070b08be63c059f
nalale <alelazk@yandex.ru>
2019-09-17 17:55:51 +0300

    Add DisplayEcu Form

---
+++ Diag/Ecu/Display_ECU.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoadService
+{
+	public class Display_ECU : ECU
+	{
+		const int REPEATER_TABLE_SIZE = 20;
+
+		const int TABLE_CNT	=	6;
+
+		
+		public CodingData_t Data;
+        List<DiagnosticData> _diagData;
+        int EcuAddress;
+
+        public ushort ConfigLenght;		
+
+		public Display_ECU(EcuModelId modelId, byte address)
+			: base(modelId, address)
+		{
+			Data = new CodingData_t();
+            _diagData = new List<DiagnosticData>();
+            Data.MotorRpm = new short[TABLE_CNT*2];
+			Data.SoC = new short[TABLE_CNT*2];
+			Data.TrimPosition = new short[TABLE_CNT*2];			
+
+            EcuAddress = address;
+            SetDiagData();
+        }
+
+
+		#region Config struct
+        
+		[StructLayout(LayoutKind.Sequential, Pack = 4, CharSet = CharSet.Unicode)]
+		public unsafe struct CodingData_t
+		{
+			// Общие
+			public byte DiagnosticID;
+			public byte Index;                   // Порядковый номер ECU.
+			
+			// Обороты мотора
+			[MarshalAs(UnmanagedType.ByValArray, SizeConst = TABLE_CNT*2)]
+			public short[] MotorRpm;
+			// Уровень заряда накопителя
+			[MarshalAs(UnmanagedType.ByValArray, SizeConst = TABLE_CNT*2)]
+			public short[] SoC;
+			// Положение трим
+			[MarshalAs(UnmanagedType.ByValArray, SizeConst = TABLE_CNT*2)]
+			public short[] TrimPosition;
+            // Удельный расход
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = TABLE_CNT*2)]
+            public short[] SpecPower;
+
+            public ushort PowerOffDelay_ms;
+            public ushort KeyOffTime_ms;            
+
+            public short addition_1;
+			public short addition_2;
+			public short addition_3;
+			public short addition_4;
+			public short addition_5;
+			public short addition_6;
+			public short addition_7;
+            
+			// Контрольная сумма
+			public ushort CRC;		
+		}
+        #endregion
+
+        #region diag data
+
+        private void SetDiagData()
+        {
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didDateTime, "Время системы", 1, true, 1, Converter_SystemTime));
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didInOutState, "Порты ввода вывода", 1, true, 1, Converter_DischargeMask));
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didMainState, "Режим работы блока", 1, true, 1, Converter_EcuState));
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didSubState, "Подрежим работы блока", 1, true, 1, null));
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didEcuVoltage, "Напряжение ECU", 1, true, 1, null));
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didPowerManagmentState, "Режим управления питанием", 1, true, 1, PowerManagerStateToString));
+
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didPwmOutState, "Выход ШИМ", 1, true, 4, null));
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didVoltageSensor1, "Датчик напряжения 1", 1, true, 1, null));
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didVoltageSensor2, "Датчик напряжения 2", 1, true, 1, null));
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didVoltageSensor3, "Датчик напряжения 3", 1, true, 1, null));
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didVoltageSensor4, "Датчик напряжения 4", 1, true, 1, null));
+
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didCurrentSensor1, "Датчик тока 1", 1, true, 1, null));
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didCurrentSensor2, "Датчик тока 2", 1, true, 1, null));
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didCurrentSensor3, "Датчик тока 3", 1, true, 1, null));
+
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didFaults_Actual, "Активные ошибки", 1, true, (byte)FaultCodes.dtc_Count, Converter_Faults));
+            _diagData.Add(new DiagnosticData((ushort)ObjectsIndex_e.didFaults_History, "Сохраненные ошибки", 1, true, (byte)FaultCodes.dtc_Count, Converter_Faults));
+        }
+
+        #endregion
+
+        public override List<DiagnosticData> GetDiagnosticSets()
+        {
+            foreach (DiagnosticData dd in _diagData)
+                dd.Value.Clear();
+
+            return _diagData;
+        }
+
+        public override async Task<List<ResponseData_ReadDataByIdentifier>> GetEcuInfo()
+        {
+            return await Global.diag.ReadDataByIDs((byte)EcuAddress, new byte[] { (byte)ObjectsIndex_e.didEcuInfo });
+        }
+
+		public override async Task<bool> ClearFaults()
+		{
+			return await Global.diag.WriteDataByID((byte)EcuAddress, (byte)ObjectsIndex_e.didFaults_History, new byte[] { 0 });
+		}
+
+		#region ObjectIndexes
+
+		public enum ObjectsIndex_e
+        {
+            didEcuInfo = 0,
+            didConfigStructIndex,
+            didDateTime,
+            didMainState,
+            didSubState,
+            didInOutState,
+            didEcuVoltage,
+            didPowerManagmentState,
+
+            didVoltageSensor1 = 10,
+            didVoltageSensor2,
+            didVoltageSensor3,
+            didVoltageSensor4,
+            didVoltageSensor5,
+            didVoltageSensor6,
+            didVoltageSensor7,
+            didVoltageSensor8,
+
+            didCurrentSensor1,
+            didCurrentSensor2,
+            didCurrentSensor3,
+            didCurrentSensor4,
+            
+            didPwmOutState,
+
+            // Диагностика ошибок
+            didFaults_Actual = 100,
+            didFaults_History,
+
+            didFaults_FreezeFrame,
+
+        };
+
+        public enum FaultCodes
+        {
+            dtc_General_EcuConfig = 0,
+            dtc_General_EcuSupplyOutOfRange,
+            dtc_General_UnexpectedPowerOff,
+            dtc_General_Interlock,
+            dtc_General_EcuDataTimeNotCorrect,
+
+            // Таймаут CAN
+            dtc_CAN_ExtCan,
+            dtc_CAN_PCAN,
+
+            dtc_PwmCircuit_1,
+            dtc_PwmCircuit_2,
+            dtc_PwmCircuit_3,
+            dtc_MeasuringCircuit,
+            dtc_PowerSupplyCircuit,
+
+
+            dtc_Count
+        }
+
+        string FaultCodeToString(FaultCodes Code)
+        {
+            string fault_desc;
+            switch (Code)
+            {
+                case FaultCodes.dtc_General_EcuConfig:
+                    fault_desc = "Ошибка памяти";
+                    break;
+                case FaultCodes.dtc_General_EcuDataTimeNotCorrect:
+                    fault_desc = "Некорректное дата/время";
+                    break;
+                case FaultCodes.dtc_General_EcuSupplyOutOfRange:
+                    fault_desc = "Напряжение питания";
+                    break;
+                case FaultCodes.dtc_General_Interlock:
+                    fault_desc = "Интерлок";
+                    break;
+                case FaultCodes.dtc_General_UnexpectedPowerOff:
+                    fault_desc = "Неожиданное отключение питания";
+                    break;
+                case FaultCodes.dtc_CAN_ExtCan:
+                    fault_desc = "Оффлайн CAN 2";
+                    break;
+                case FaultCodes.dtc_CAN_PCAN:
+                    fault_desc = "Оффлайн CAN 1";
+                    break;
+                case FaultCodes.dtc_PwmCircuit_1:
+                    fault_desc = "Цепи ШИМ 1";
+                    break;
+                case FaultCodes.dtc_PwmCircuit_2:
+                    fault_desc = "Цепи ШИМ 2";
+                    break;
+                case FaultCodes.dtc_PwmCircuit_3:
+                    fault_desc = "Цепи ШИМ 3";
+                    break;
+                case FaultCodes.dtc_MeasuringCircuit:
+                    fault_desc = "Цепи измерения АЦП";
+                    break;
+                case FaultCodes.dtc_PowerSupplyCircuit:
+                    fault_desc = "Цепи питания ECU";
+                    break;
+
+                default:
+                    fault_desc = Code.ToString();
+                    break;
+            }
+
+            return fault_desc;
+        }
+
+		string PowerManagerStateToString(int Code)
+        {
+            string fault_desc;
+            switch (Code)
+            {
+                case 0:
+                    fault_desc = "Питание отсутствует";
+                    break;
+                case 1:
+                    fault_desc = "Питание присутствует";
+                    break;
+				case 2:
+					fault_desc = "Питание присутствует. Поддержка питания";
+					break;
+                case 3:
+                    fault_desc = "Выключение";
+                    break;
+                default:
+                    fault_desc = Code.ToString();
+                    break;
+            }
+
+            return fault_desc;
+        }
+		#endregion
+
+		#region DiagConverter
+
+		string Converter_EcuState(int Code)
+		{
+			switch (Code)
+			{
+				case 0:
+					return "Инициализация";
+				case 1:
+					return "Рабочий ";
+				case 2:
+					return "Выключение";
+				case 3:
+					return "Ошибка";
+				case 4:
+					return "Заряд";
+				default:
+					return Code.ToString();
+			}
+		}
+
+		string Converter_Faults(int Code)
+        {
+            FaultCodes fault_id = (FaultCodes)((Code & 0xff00) >> 8);
+            int fault_cat = Code & 0xff;
+
+            string fault_desc = FaultCodeToString(fault_id);
+            string cat_desc = DTC_Category.ToString((DTC_Category.Code)fault_cat);
+
+            return fault_desc + ", " + cat_desc;
+        }
+
+        string Converter_DischargeMask(int Code)
+        {
+            return Convert.ToString(Code, 2).PadLeft(32, '0');
+        }
+
+        string Converter_SystemTime(int Code)
+        {
+            DateTime dt = new DateTime();
+            dt.AddSeconds(Code);
+
+            return dt.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ Forms/Display_ECUCoding.xaml.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Runtime.InteropServices;
+
+namespace BoadService
+{
+	/// <summary>
+	/// Interaction logic for Mcu_ECUCoding.xaml
+	/// </summary>
+	public partial class Display_ECUCoding : UserControl, IEcu_Coding
+	{
+        Display_ECU dEcu;	
+
+		public Display_ECUCoding()
+		{
+			InitializeComponent();				
+
+			dEcu = Global.EcuList.CurrentEcu as Display_ECU;
+		}
+
+		public ECU CurrentEcu { get; set; }
+
+
+		#region General		
+
+		// Заполнение структуры данными из формы
+		bool FormToStruct()
+		{
+			// Общие
+			dEcu.Data.DiagnosticID = Convert.ToByte(tbDiagnosticAddress.Text);
+			dEcu.Data.Index = Convert.ToByte(tbModuleID.Text);
+
+			// Батареи
+			dEcu.Data.MotorRpm[0] = Convert.ToByte(tbSpeed1.Text);
+			dEcu.Data.MotorRpm[1] = Convert.ToByte(tbSpeed2.Text);
+			dEcu.Data.MotorRpm[2] = Convert.ToByte(tbSpeed3.Text);
+			dEcu.Data.MotorRpm[3] = Convert.ToByte(tbSpeed4.Text);
+			dEcu.Data.MotorRpm[4] = Convert.ToByte(tbSpeed5.Text);
+			dEcu.Data.MotorRpm[5] = Convert.ToByte(tbSpeed6.Text);
+            dEcu.Data.MotorRpm[6] = Convert.ToByte(tbMin1.Text);
+            dEcu.Data.MotorRpm[7] = Convert.ToByte(tbMin2.Text);
+            dEcu.Data.MotorRpm[8] = Convert.ToByte(tbMin3.Text);
+            dEcu.Data.MotorRpm[9] = Convert.ToByte(tbMin4.Text);
+            dEcu.Data.MotorRpm[10] = Convert.ToByte(tbMin5.Text);
+            dEcu.Data.MotorRpm[11] = Convert.ToByte(tbMin6.Text);
+
+            dEcu.Data.SoC[0] = Convert.ToByte(tbSoc1.Text);
+			dEcu.Data.SoC[1] = Convert.ToByte(tbSoc2.Text);
+			dEcu.Data.SoC[2] = Convert.ToByte(tbSoc3.Text);
+			dEcu.Data.SoC[3] = Convert.ToByte(tbSoc4.Text);
+            dEcu.Data.SoC[4] = Convert.ToByte(tbSoc5.Text);
+            dEcu.Data.SoC[5] = Convert.ToByte(tbSoc6.Text);
+            dEcu.Data.SoC[6] = Convert.ToByte(tbSocPwm1.Text);
+            dEcu.Data.SoC[7] = Convert.ToByte(tbSocPwm2.Text);
+            dEcu.Data.SoC[8] = Convert.ToByte(tbSocPwm3.Text);
+            dEcu.Data.SoC[9] = Convert.ToByte(tbSocPwm4.Text);
+            dEcu.Data.SoC[10] = Convert.ToByte(tbSocPwm5.Text);
+            dEcu.Data.SoC[11] = Convert.ToByte(tbSocPwm6.Text);
+
+            dEcu.Data.TrimPosition[0] = Convert.ToByte(tbTrim1.Text);
+            dEcu.Data.TrimPosition[1] = Convert.ToByte(tbTrim2.Text);
+            dEcu.Data.TrimPosition[2] = Convert.ToByte(tbTrim3.Text);
+            dEcu.Data.TrimPosition[3] = Convert.ToByte(tbTrim4.Text);
+            dEcu.Data.TrimPosition[4] = Convert.ToByte(tbTrim5.Text);
+            dEcu.Data.TrimPosition[5] = Convert.ToByte(tbTrim6.Text);
+            dEcu.Data.TrimPosition[6] = Convert.ToByte(tbTrimPwm1.Text);
+            dEcu.Data.TrimPosition[7] = Convert.ToByte(tbTrimPwm2.Text);
+            dEcu.Data.TrimPosition[8] = Convert.ToByte(tbTrimPwm3.Text);
+            dEcu.Data.TrimPosition[9] = Convert.ToByte(tbTrimPwm4.Text);
+            dEcu.Data.TrimPosition[10] = Convert.ToByte(tbTrimPwm5.Text);
+            dEcu.Data.TrimPosition[11] = Convert.ToByte(tbTrimPwm6.Text);
+
+            dEcu.Data.SpecPower[0] = Convert.ToByte(tbCons1.Text);
+            dEcu.Data.SpecPower[1] = Convert.ToByte(tbCons2.Text);
+            dEcu.Data.SpecPower[2] = Convert.ToByte(tbCons3.Text);
+            dEcu.Data.SpecPower[3] = Convert.ToByte(tbCons4.Text);
+            dEcu.Data.SpecPower[4] = Convert.ToByte(tbCons5.Text);
+            dEcu.Data.SpecPower[5] = Convert.ToByte(tbCons6.Text);
+            dEcu.Data.SpecPower[6] = Convert.ToByte(tbConsPwm1.Text);
+            dEcu.Data.SpecPower[7] = Convert.ToByte(tbConsPwm2.Text);
+            dEcu.Data.SpecPower[8] = Convert.ToByte(tbConsPwm3.Text);
+            dEcu.Data.SpecPower[9] = Convert.ToByte(tbConsPwm4.Text);
+            dEcu.Data.SpecPower[10] = Convert.ToByte(tbConsPwm5.Text);
+            dEcu.Data.SpecPower[11] = Convert.ToByte(tbConsPwm6.Text);
+
+
+            dEcu.Data.PowerOffDelay_ms = Convert.ToUInt16(tbPowerOffDelay.Text);
+            dEcu.Data.KeyOffTime_ms = Convert.ToUInt16(tbKeyOffTime.Text);
+
+            return true;
+		}
+		void StructToForm()
+		{
+			tbDiagnosticAddress.Text = dEcu.Data.DiagnosticID.ToString();
+
+			// Общие
+			tbDiagnosticAddress .Text = dEcu.Data.DiagnosticID.ToString();
+			tbModuleID.Text = dEcu.Data.Index.ToString();
+
+            tbSpeed1.Text = Convert.ToString(dEcu.Data.MotorRpm[0]);
+            tbSpeed2.Text = Convert.ToString(dEcu.Data.MotorRpm[1]);
+            tbSpeed3.Text = Convert.ToString(dEcu.Data.MotorRpm[2]);
+            tbSpeed4.Text = Convert.ToString(dEcu.Data.MotorRpm[3]);
+            tbSpeed5.Text = Convert.ToString(dEcu.Data.MotorRpm[4]);
+            tbSpeed6.Text = Convert.ToString(dEcu.Data.MotorRpm[5]);
+            tbMin1.Text = Convert.ToString(dEcu.Data.MotorRpm[6]);
+            tbMin2.Text = Convert.ToString(dEcu.Data.MotorRpm[7]);
+            tbMin3.Text = Convert.ToString(dEcu.Data.MotorRpm[8]);
+            tbMin4.Text = Convert.ToString(dEcu.Data.MotorRpm[9]);
+            tbMin5.Text = Convert.ToString(dEcu.Data.MotorRpm[10]);
+            tbMin6.Text = Convert.ToString(dEcu.Data.MotorRpm[11]);
+
+            tbSoc1.Text = Convert.ToString(dEcu.Data.SoC[0]);
+            tbSoc2.Text = Convert.ToString(dEcu.Data.SoC[1]);
+            tbSoc3.Text = Convert.ToString(dEcu.Data.SoC[2]);
+            tbSoc4.Text = Convert.ToString(dEcu.Data.SoC[3]);
+            tbSoc5.Text = Convert.ToString(dEcu.Data.SoC[4]);
+            tbSoc6.Text = Convert.ToString(dEcu.Data.SoC[5]);
+            tbSocPwm1.Text = Convert.ToString(dEcu.Data.SoC[6]);
+            tbSocPwm2.Text = Convert.ToString(dEcu.Data.SoC[7]);
+            tbSocPwm3.Text = Convert.ToString(dEcu.Data.SoC[8]);
+            tbSocPwm4.Text = Convert.ToString(dEcu.Data.SoC[9]);
+            tbSocPwm5.Text = Convert.ToString(dEcu.Data.SoC[10]);
+            tbSocPwm6.Text = Convert.ToString(dEcu.Data.SoC[11]);
+
+            tbTrim1.Text = Convert.ToString(dEcu.Data.TrimPosition[0]);
+            tbTrim2.Text = Convert.ToString(dEcu.Data.TrimPosition[1]);
+            tbTrim3.Text = Convert.ToString(dEcu.Data.TrimPosition[2]);
+            tbTrim4.Text = Convert.ToString(dEcu.Data.TrimPosition[3]);
+            tbTrim5.Text = Convert.ToString(dEcu.Data.TrimPosition[4]);
+            tbTrim6.Text = Convert.ToString(dEcu.Data.TrimPosition[5]);
+            tbTrimPwm1.Text = Convert.ToString(dEcu.Data.TrimPosition[6]);
+            tbTrimPwm2.Text = Convert.ToString(dEcu.Data.TrimPosition[7]);
+            tbTrimPwm3.Text = Convert.ToString(dEcu.Data.TrimPosition[8]);
+            tbTrimPwm4.Text = Convert.ToString(dEcu.Data.TrimPosition[9]);
+            tbTrimPwm5.Text = Convert.ToString(dEcu.Data.TrimPosition[10]);
+            tbTrimPwm6.Text = Convert.ToString(dEcu.Data.TrimPosition[11]);
+
+            tbCons1.Text = Convert.ToString(dEcu.Data.SpecPower[0]);
+            tbCons2.Text = Convert.ToString(dEcu.Data.SpecPower[1]);
+            tbCons3.Text = Convert.ToString(dEcu.Data.SpecPower[2]);
+            tbCons4.Text = Convert.ToString(dEcu.Data.SpecPower[3]);
+            tbCons5.Text = Convert.ToString(dEcu.Data.SpecPower[4]);
+            tbCons6.Text = Convert.ToString(dEcu.Data.SpecPower[5]);
+            tbConsPwm1.Text = Convert.ToString(dEcu.Data.SpecPower[6]);
+            tbConsPwm2.Text = Convert.ToString(dEcu.Data.SpecPower[7]);
+            tbConsPwm3.Text = Convert.ToString(dEcu.Data.SpecPower[8]);
+            tbConsPwm4.Text = Convert.ToString(dEcu.Data.SpecPower[9]);
+            tbConsPwm5.Text = Convert.ToString(dEcu.Data.SpecPower[10]);
+            tbConsPwm6.Text = Convert.ToString(dEcu.Data.SpecPower[11]);
+
+            tbPowerOffDelay.Text = dEcu.Data.PowerOffDelay_ms.ToString();
+            tbKeyOffTime.Text = dEcu.Data.KeyOffTime_ms.ToString();
+
+        }
+
+		#endregion
+
+
+
+		#region Coding
+
+
+		async public Task<bool> Download()
+		{
+			IntPtr ptr;
+			int ProfileSize = Marshal.SizeOf(typeof(General_ECU.CodingData_t));
+			
+			ptr = Marshal.AllocHGlobal(ProfileSize);
+			byte[] buf = await Global.diag.ReadDataByID((byte)CurrentEcu.Address, 0);
+
+			Marshal.Copy(buf, 0, ptr, ProfileSize);
+			dEcu.Data = (Display_ECU.CodingData_t)Marshal.PtrToStructure(ptr, typeof(Display_ECU.CodingData_t));
+
+			ushort loc_crc = Tools.CRC16(buf, ProfileSize / 4 - 1);
+			if (dEcu.Data.CRC == loc_crc)
+			{
+				StructToForm();
+			}
+			else
+				return false;
+
+			return true; 
+
+
+		}
+
+		public async Task<ReadDataResult_e> Upload()
+		{
+			IntPtr ptr;
+			if (FormToStruct())
+			{				
+				int ProfileSize = Marshal.SizeOf(typeof(General_ECU.CodingData_t));		
+				
+				ptr = Marshal.AllocHGlobal(ProfileSize);
+				Marshal.StructureToPtr(dEcu.Data, ptr, false);
+
+				byte[] buf = new byte[ProfileSize];				
+
+				Marshal.Copy(ptr, buf, 0, buf.Length);
+				dEcu.Data.CRC = Tools.CRC16(buf, buf.Length / 4 - 1);
+
+				Marshal.StructureToPtr(dEcu.Data, ptr, false);
+				Marshal.Copy(ptr, buf, 0, buf.Length);				
+
+				Marshal.FreeHGlobal(ptr);
+
+				bool res = await Global.diag.WriteDataByID((byte)CurrentEcu.Address, (byte)Display_ECU.ObjectsIndex_e.didConfigStructIndex, buf);
+
+                return (res) ? ReadDataResult_e.RES_SUCCESSFUL : ReadDataResult_e.RES_OTHER_ERROR;
+            }
+            return ReadDataResult_e.RES_INCORRECT_DATA;
+        }
+
+		public void Open(string path)
+		{
+
+		}
+
+		public void Save(string path)
+		{
+
+		}
+
+		#endregion
+
+		private void apply_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
